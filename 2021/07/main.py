33588cea4fa140d3150a90c007e4c661117beeb6
Robbie <robbiebsinger@gmail.com>
2021-12-10 09:38:17 +0000

    Add day 7

---
+++ 2021/07/main.py
@@ -0,0 +1,19 @@
+import statistics
+import math
+
+with open('input') as i:
+    crabs = [int(x) for x in i.read().split(',')]
+
+def task1():
+  median = round(statistics.median(crabs))
+  fuel_used = sum([abs(median - x) for x in crabs])
+  print(fuel_used)
+
+def task2():
+  mean = math.floor(statistics.mean(crabs))
+  steps = [range(abs(mean - x)) for x in crabs]
+  fuel_steps = [[x+1 for x in step] for step in steps]
+  total_fuel = sum([sum(step) for step in fuel_steps])
+  print(total_fuel)
+
+task2()
